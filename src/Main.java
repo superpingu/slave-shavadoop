47b6509410898ba1baeb468b58429f88b2da6ce3
superpingu <arnaudbonetti@me.com>
2016-06-01 15:58:49 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.print("Waiting 10s ...");
+        Thread.sleep(10000);
+        System.out.println("OK");
+    }
+}
